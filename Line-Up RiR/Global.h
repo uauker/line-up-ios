3f4d529a7269168ef49f80baf674e3e25d265f86
Paulo Guilherme <uauker@gmail.com>
2013-07-25 20:07:51 -0300

    incluido global

---
+++ Line-Up RiR/Global.h
@@ -0,0 +1,9 @@
+//
+//  Global.h
+//  Line-Up RiR
+//
+//  Created by Uauker on 7/25/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#define K_ARRAY_EVENTS [NSArray arrayWithObjects:@"SEXTA 13/09", @"SÁBADO 14/09", @"DOMINGO 15/09", @"QUINTA 19/09", @"SEXTA 20/09", @"SÁBADO 21/09", @"DOMINGO 22/09", nil]
