f62763b52d3fcbb845da3cbb1d20f67a13f9172b
Alexandre Marones <alexandre.marones@gmail.com>
2013-07-25 13:41:18 -0300

    criando LineUpVC e iniciando criacao da tela no Storyboard

---
+++ Line-Up RiR/Classes/ViewControllers/LineUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  LineUpViewController.h
+//  Line-Up RiR
+//
+//  Created by Alexandre Santos on 7/25/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LineUpViewController : UIViewController
+
+@end
---
+++ Line-Up RiR/Classes/ViewControllers/LineUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  LineUpViewController.m
+//  Line-Up RiR
+//
+//  Created by Alexandre Santos on 7/25/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "LineUpViewController.h"
+
+@interface LineUpViewController ()
+
+@end
+
+@implementation LineUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
