ac6e7d654dedd56963332f79a72c28162a160004
Paulo Guilherme <uauker@gmail.com>
2013-07-30 21:42:19 -0300

    model do fb

---
+++ Line-Up RiR/Models/FBUser.h
@@ -0,0 +1,18 @@
+//
+//  FBUser.h
+//  Line-Up RiR
+//
+//  Created by Uauker on 7/30/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBUser : NSObject
+
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *eventDate;
+@property (nonatomic, copy) NSString *username;
+
+@end
---
+++ Line-Up RiR/Models/FBUser.m
@@ -0,0 +1,14 @@
+//
+//  FBUser.m
+//  Line-Up RiR
+//
+//  Created by Uauker on 7/30/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "FBUser.h"
+
+@implementation FBUser
+
+
+@end
