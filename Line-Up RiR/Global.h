2fab65d5ab06d8ca209a13698f3634613f6a76e8
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-02 00:36:29 -0300

    aficionado url q repera amigos do usuario que vao a um determinado evento

--- Line-Up RiR/Global.h
+++ Line-Up RiR/Global.h
@@ -9,3 +9,9 @@
 #define K_ARRAY_EVENTS [NSArray arrayWithObjects:@"SEXTA 13/09", @"SÁBADO 14/09", @"DOMINGO 15/09", @"QUINTA 19/09", @"SEXTA 20/09", @"SÁBADO 21/09", @"DOMINGO 22/09", nil]
 
 #define K_ARRAY_PALCOS [NSArray arrayWithObjects:@"PALCO MUNDO", @"PALCO SUNSET", @"ELETRÔNICA", @"ROCK STREET", nil]
+
+
+//---------------------------------------------------------------------------------------------------------
+// HEROKU URL
+//---------------------------------------------------------------------------------------------------------
+#define K_HEROKU_FRINDS_FROM_USER_AND_EVENT_DATE @"http://line-up-rails.herokuapp.com/api/facebook/v1/events/1?facebook_users_id=%@1&event_date=%@2"
