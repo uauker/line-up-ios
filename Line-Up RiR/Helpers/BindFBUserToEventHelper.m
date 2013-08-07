0e1b715406d0223cd7fb8ab4ae65885ba2e16ef9
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-06 22:34:40 -0300

    criando helper para realizar bind do FBUser para Events

---
+++ Line-Up RiR/Helpers/BindFBUserToEventHelper.m
@@ -0,0 +1,13 @@
+//
+//  BindFBUserToEventHelper.m
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/6/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "BindFBUserToEventHelper.h"
+
+@implementation BindFBUserToEventHelper
+
+@end
