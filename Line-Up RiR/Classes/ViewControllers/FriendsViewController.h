abeadce9fc178721dd596c52aa12deb5a586d0bd
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-08 20:04:26 -0300

    criando FriendsVC

---
+++ Line-Up RiR/Classes/ViewControllers/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/8/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
---
+++ Line-Up RiR/Classes/ViewControllers/FriendsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FriendsViewController.m
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/8/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "FriendsViewController.h"
+
+@interface FriendsViewController ()
+
+@end
+
+@implementation FriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
