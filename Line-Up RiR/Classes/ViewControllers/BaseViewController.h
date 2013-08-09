f48f8df7dc1c6c6d7dbd8b93a2676ac3134ce327
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-08 21:05:36 -0300

    adicionando BaseVC e as libraries SystemConfiguration.framework e CoreDate.framework

---
+++ Line-Up RiR/Classes/ViewControllers/BaseViewController.h
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.h
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/8/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GAITrackedViewController.h"
+
+@interface BaseViewController : GAITrackedViewController
+
+@end
---
+++ Line-Up RiR/Classes/ViewControllers/BaseViewController.m
@@ -0,0 +1,42 @@
+//
+//  BaseViewController.m
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/8/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backPop:(id)sender {
+    [[self navigationController] popViewControllerAnimated:YES];
+}
+
+@end
