6d3aa87545d9312bb59a5a1a4d1b83e302f50cbf
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-10 14:04:54 -0300

    criando helper para mensagem de erro

---
+++ Line-Up RiR/Helpers/ErrorMessageHelper.h
@@ -0,0 +1,13 @@
+//
+//  ErrorMessageHelper.h
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/10/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ErrorMessageHelper : NSObject
+
+@end
---
+++ Line-Up RiR/Helpers/ErrorMessageHelper.m
@@ -0,0 +1,13 @@
+//
+//  ErrorMessageHelper.m
+//  Line-Up RiR
+//
+//  Created by Alexandre Marones on 8/10/13.
+//  Copyright (c) 2013 Uauker Inc. All rights reserved.
+//
+
+#import "ErrorMessageHelper.h"
+
+@implementation ErrorMessageHelper
+
+@end
